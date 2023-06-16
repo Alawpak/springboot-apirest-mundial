f048a5684fddb7084213ad005b53208754d2caab
Alawpak <alan281@hotmail.es>
2023-06-16 13:19:16 -0600

    Se agregan objetos para login

--- src/main/java/com/springboot/apirest/mundial/service/dto/user/login/LoginRequest.java
+++ src/main/java/com/springboot/apirest/mundial/service/dto/user/login/LoginRequest.java
@@ -1,4 +1,10 @@
 package com.springboot.apirest.mundial.service.dto.user.login;
 
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    private String email;
+    private String password;
 }
--- src/main/java/com/springboot/apirest/mundial/service/dto/user/login/LoginResponse.java
+++ src/main/java/com/springboot/apirest/mundial/service/dto/user/login/LoginResponse.java
@@ -1,4 +1,13 @@
 package com.springboot.apirest.mundial.service.dto.user.login;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.springboot.apirest.mundial.service.dto.user.register.RegisteredDataUser;
+import lombok.Data;
+
+@Data
 public class LoginResponse {
+    private String status;
+    @JsonProperty("data")
+    private RegisteredDataUser registeredDataUser;
+
 }
