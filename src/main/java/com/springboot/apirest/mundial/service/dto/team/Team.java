0171af258032dd28af195888ef21006a07835755
Alawpak <alan281@hotmail.es>
2023-06-16 13:29:46 -0600

    se agregan objetos para team

--- src/main/java/com/springboot/apirest/mundial/service/dto/team/Team.java
+++ src/main/java/com/springboot/apirest/mundial/service/dto/team/Team.java
@@ -1,4 +1,19 @@
 package com.springboot.apirest.mundial.service.dto.team;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class Team {
+    private String id;
+    private String _id;
+    private String flag;
+    @JsonProperty("iso2")
+    private String country;
+    @JsonProperty("name_en")
+    private String name;
+    @JsonProperty("fifa_code")
+    private String fifaCode;
+    @JsonProperty("groups")
+    private String group;
 }
--- src/main/java/com/springboot/apirest/mundial/service/dto/team/TeamResponse.java
+++ src/main/java/com/springboot/apirest/mundial/service/dto/team/TeamResponse.java
@@ -1,4 +1,13 @@
 package com.springboot.apirest.mundial.service.dto.team;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class TeamResponse {
+    private String status;
+    @JsonProperty("data")
+    private List<Team> teams;
 }
