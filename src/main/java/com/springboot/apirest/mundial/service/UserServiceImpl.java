f77689459f306b0207c526877d8fa45b4ecb49d5
Alawpak <alan281@hotmail.es>
2023-06-16 16:24:32 -0600

    Se agrega metodos para login y register

--- src/main/java/com/springboot/apirest/mundial/service/UserServiceImpl.java
+++ src/main/java/com/springboot/apirest/mundial/service/UserServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class UserServiceImpl extends AbstractClient implements UserService {
 
-
     protected UserServiceImpl(RestTemplate restTemplate) {
         super(restTemplate);
     }
@@ -39,6 +38,16 @@ public class UserServiceImpl extends AbstractClient implements UserService {
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
-        return null;
+
+        String uri = baseUrl + "/user/login";
+        log.info("Going to login {}", loginRequest);
+        ResponseEntity<LoginResponse> response = restTemplate.postForEntity(uri, loginRequest, LoginResponse.class);
+        if (response.getStatusCode().is2xxSuccessful()) {
+            log.info("Successfully user creation: {}", response.getBody().getStatus());
+            return response.getBody();
+        }
+        log.error("Error in user login - httpStatus was: {}", response.getStatusCode());
+        throw new RuntimeException("Error");
+
     }
 }
