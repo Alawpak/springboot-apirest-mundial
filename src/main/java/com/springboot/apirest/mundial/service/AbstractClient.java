82592af132a2247974a3f5d626e8c1ece6a93e6b
Alawpak <alan281@hotmail.es>
2023-06-16 12:30:18 -0600

    se agrega metodo para headers

--- src/main/java/com/springboot/apirest/mundial/service/AbstractClient.java
+++ src/main/java/com/springboot/apirest/mundial/service/AbstractClient.java
@@ -1,6 +1,7 @@
 package com.springboot.apirest.mundial.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
 //Clase que contiene cosas comunes que van a utilizar todos los clientes que se conectan a la api rest
@@ -13,5 +14,11 @@ public abstract class AbstractClient {
 
     protected AbstractClient(RestTemplate restTemplate){ this.restTemplate = restTemplate; }
 
+    protected HttpHeaders buildAuthToken(String token){
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", "Bearer " + token);
 
+        return headers;
+    }
 }
