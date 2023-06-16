ee52bab95ffb28a42ceff18d3c99ee4119088a05
Alawpak <alan281@hotmail.es>
2023-06-16 12:25:25 -0600

    se agrega clase abstracta AbstractClient

--- src/main/java/com/springboot/apirest/mundial/service/AbstractClient.java
+++ src/main/java/com/springboot/apirest/mundial/service/AbstractClient.java
@@ -1,4 +1,17 @@
 package com.springboot.apirest.mundial.service;
 
-public class AbstractClient {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+//Clase que contiene cosas comunes que van a utilizar todos los clientes que se conectan a la api rest
+public abstract class AbstractClient {
+
+    @Value("${worldcup.base-url}")
+    protected String baseUrl;
+
+    protected final RestTemplate restTemplate;
+
+    protected AbstractClient(RestTemplate restTemplate){ this.restTemplate = restTemplate; }
+
+
 }
